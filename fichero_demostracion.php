d6de0c6807a6906b2db5115fe5169f8e62ba0b46
Manuel <manueljesusjimenezromero@gmail.com>
2026-09-20 17:07:15 +0200

    modphp

--- fichero_demostracion.php
+++ fichero_demostracion.php
@@ -11,6 +11,6 @@
         <?php echo htmlspecialchars(date('d/m/Y')); ?>
         a las <?php echo htmlspecialchars(date('H:m:s')); ?>
         </p>
-        <h1><?php echo htmlspecialchars('Se imprimen variables en html para evitar ataques XSS'); ?></h1>
+        <p><Strong><?php echo htmlspecialchars('Se imprimen variables en html para evitar ataques XSS'); ?></Strong></p>
     </body>
 </html>
