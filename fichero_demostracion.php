85b7e31f61948c8184b8f790724d4eabf9420e70
Manuel <manueljesusjimenezromero@gmail.com>
2026-09-20 17:05:56 +0200

    tareaTerminada

--- fichero_demostracion.php
+++ fichero_demostracion.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="es">
     <head>
-        <title>Mi tienda</title>
+        <title>Demostracion de web dinamica</title>
     </head>
     <body>
+        <h1>Demostracion de web dinamica</h1>
         <br>
+
         <p>Has entrado el dia:
         <?php echo htmlspecialchars(date('d/m/Y')); ?>
         a las <?php echo htmlspecialchars(date('H:m:s')); ?>
